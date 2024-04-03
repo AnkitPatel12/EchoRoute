290537eded0cb57d6667a55f4ac341e16c24a7ad
AnkitPatel12 <ankit.p124234@gmail.com>
2024-04-03 09:57:35 -0500

    Initial Commit

---
+++ EchoRoute/EchoRouteApp.swift
@@ -0,0 +1,17 @@
+//
+//  EchoRouteApp.swift
+//  EchoRoute
+//
+//  Created by Ankit Patel on 4/3/24.
+//
+
+import SwiftUI
+
+@main
+struct EchoRouteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
